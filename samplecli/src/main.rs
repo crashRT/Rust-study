563ce8601bb38b1dbe58bab9443aca0f143a8932
crashRT <crashrt.farbauti@gmail.com>
2023-02-11 01:35:28 +0900

    clap Builder パターン

--- samplecli/src/main.rs
+++ samplecli/src/main.rs
@@ -1,6 +1,29 @@
-use std::env;
+use clap::{App, Arg};
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    println!("{:?}", args);
-}
+    let matches = App::new("My RPN program")
+        .version("1.0.0")
+        .author("crashRT")
+        .about("Super awesome sample RPN calculator")
+        .arg(
+            Arg::new("formula_file")
+                .value_name("FILE")
+                .index(1)
+                .required(false),
+        )
+        .arg(
+            Arg::new("verbose")
+                .short('v')
+                .long("verbose")
+                .required(false),
+            )
+            .get_matches();
+    
+        match matches.value_of("formula_file") {
+            Some(file) => println!("File specipied: {}", file),
+            None => println!("No file specified"),
+        }
+
+        let verbose = matches.is_present("verbose");
+        println!("is verbosity specified?: {}", verbose);
+    }
