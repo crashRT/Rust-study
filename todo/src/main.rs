2c05c5e24eff8dc8bde1f79e71a5376ff1117821
crashRT <crashrt.farbauti@gmail.com>
2023-02-15 03:36:13 +0900

    htmlの表示

--- todo/src/main.rs
+++ todo/src/main.rs
@@ -1,9 +1,47 @@
-use actix_web::{get, App, HttpResponse, HttpServer};
+use actix_web::{get, App, HttpResponse, HttpServer, ResponseError};
+use thiserror::Error;
+use askama::Template;
+
+#[derive(Error, Debug)]
+enum MyError {
+    #[error("Failed to render HTML")]
+    AskamaError(#[from] askama::Error),
+}
+
+impl ResponseError for MyError {}
+
+struct TodoEntry {
+    id: u32,
+    text: String,
+}
+
+#[derive(Template)]
+#[template(path = "index.html")]
+struct IndexTemplate {
+    entries: Vec<TodoEntry>,
+}
+
+
 
 #[get("/")]
-async fn index() -> Result<HttpResponse, actix_web::Error> {
-    let response_body = "Hello world!";
-    Ok(HttpResponse::Ok().body(response_body))
+async fn index() -> Result<HttpResponse, MyError> {
+
+    // ダミーデータ
+    let mut entries = Vec::new();
+    entries.push(TodoEntry {
+        id: 1,
+        text: "Nyaan".to_string(),
+    });
+    entries.push(TodoEntry {
+        id: 2,
+        text: "Hoge".to_string(),
+    });
+
+    let html = IndexTemplate {entries};
+    let response_body = html.render()?;
+    Ok(HttpResponse::Ok()
+        .content_type("text/html")
+        .body(response_body))
 }
 
 #[actix_web::main]
